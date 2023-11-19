2bb6d38aac3d44f658ad38c4494d810b84518fc8
Maflacs <effa87@gmail.com>
2023-11-19 14:53:21 +0100

    1.0

---
+++ script.js
@@ -0,0 +1,47 @@
+// const dayNight = document.querySelector(".day-night");
+// dayNight.addEventListener("click", () =>{
+//     document.body.classList.toggle("dark");
+// })
+
+
+
+let section = document.querySelectorAll("section");
+let menu = document.querySelectorAll("nav a");
+
+window.onscroll = () => {
+  section.forEach((i) => {
+    let top = window.scrollY;
+    let offset = i.offsetTop - 150;
+    let height = i.offsetHeight;
+    let id = i.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      menu.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+reveal();
